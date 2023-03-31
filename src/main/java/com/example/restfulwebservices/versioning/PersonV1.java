1b55660bf54dce4ebeebbc42a4fa80617baffcf4
nourhan <nourhan.saeed@mobidevlabs.com>
2023-03-31 23:14:23 +0200

    hateoas

--- src/main/java/com/example/restfulwebservices/versioning/PersonV1.java
+++ src/main/java/com/example/restfulwebservices/versioning/PersonV1.java
@@ -1,2 +1,22 @@
-package com.example.restfulwebservices.versioning;public class PersonV1 {
+package com.example.restfulwebservices.versioning;
+
+public class PersonV1 {
+    private String name;
+    public PersonV1(String name) {
+        super();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public  String toString(){
+        return "PersonV1 [name = " + name + " ]";
+    }
 }
--- src/main/java/com/example/restfulwebservices/versioning/PersonV2.java
+++ src/main/java/com/example/restfulwebservices/versioning/PersonV2.java
@@ -1,2 +1,22 @@
-package com.example.restfulwebservices.versioning;public class PersonV2 {
+package com.example.restfulwebservices.versioning;
+
+
+public class PersonV2 {
+    private Name name;
+    public PersonV2(Name name) {
+        this.setName(name);
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    @Override
+    public  String toString(){
+        return "PersonV2 [name = " + name +  " ]";
+    }
 }
